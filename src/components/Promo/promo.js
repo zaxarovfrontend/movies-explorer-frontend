276eedc4e2adb710d18214d67c96e78d1390e703
Vladimir <zaxarov.frontend@gmail.com>
2021-09-22 22:56:56 +0400

    добавил разрешение 768

--- src/components/Promo/promo.js
+++ src/components/Promo/promo.js
@@ -7,7 +7,7 @@ function Promo () {
         <section className='promo'>
             <div className='promo__group'>
             <div className='promo__container'>
-            <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+            <h1 className='promo__title'>Учебный проект студента факультета Веб&#8209;разработки.</h1>
                 <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
             <NavTab/>
             </div>

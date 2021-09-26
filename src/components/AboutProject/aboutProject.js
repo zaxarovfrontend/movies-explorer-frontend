462b672abcc4f67685a4990317e8a809c1af671f
Vladimir <zaxarov.frontend@gmail.com>
2021-09-26 16:17:54 +0400

    Доделал блок Опроекте

--- src/components/AboutProject/aboutProject.js
+++ src/components/AboutProject/aboutProject.js
@@ -7,12 +7,16 @@ function AboutProject() {
             <div className='aboutProject__container'>
             <h2 className='aboutProject__header-title'>О проекте</h2>
             <div className='aboutProject__block'>
+                <div className='aboutProject__box'>
             <h3 className='aboutProject__title'>Дипломный проект включал 5 этапов</h3>
                 <p className='aboutProject__subtitle'>Составление плана, работу над бэкендом, вёрстку,
                     добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className='aboutProject__box'>
                 <h3 className='aboutProject__title'>На выполнение диплома ушло 5 недель</h3>
                 <p className='aboutProject__subtitle'>У каждого этапа был мягкий и жёсткий дедлайн,
                     которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
             </div>
              <div className='aboutProject__info'>
                  <p className='aboutProject__week'>1 неделя</p>
